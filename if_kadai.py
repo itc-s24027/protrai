dfa22da2ac25cf71aaf8facd00705e47ed957823
itc-s24027 <s24027@std.it-college.ac.jp>
2024-06-10 06:34:49 +0900

    add if_kadai.py

---
+++ if_kadai.py
@@ -0,0 +1,12 @@
+# s24027
+# scoreが60以上なら「可」、70以上なら「良」、80以上なら「優」、60未満なら「不可」と表示するプログラム
+
+score = 79
+if score >= 80:
+    print("優")
+elif score >= 70:
+    print("良")
+elif score >= 60:
+    print("可")
+elif score <= 60:
+    print("不可")
